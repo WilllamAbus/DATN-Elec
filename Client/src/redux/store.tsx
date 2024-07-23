95569e93c9c57bb73a00e66c378699f7a5db2aa0
WilllamAbus <92751063+WilllamAbus@users.noreply.github.com>
2024-07-23 19:17:30 +0700

    change --fix

--- Client/src/redux/store.tsx
+++ Client/src/redux/store.tsx
@@ -10,3 +10,34 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
+
+
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage'; 
+// import authReducer from './auth/authSlice';
+
+// // Cấu hình Redux Persist
+// const persistConfig = {
+//     key: 'root',
+//     storage, 
+// };
+
+// const persistedReducer = persistReducer(persistConfig, authReducer);
+
+// export const store = configureStore({
+//     reducer: {
+//         auth: persistedReducer,
+//     },
+//     middleware: (getDefaultMiddleware) =>
+//         getDefaultMiddleware({
+//             serializableCheck: false,
+//         }),
+// });
+
+// export const persistor = persistStore(store); 
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
