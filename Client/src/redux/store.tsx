17991466ff6f0ddd90273ae4038fd594fc93d908
WilllamAbus <92751063+WilllamAbus@users.noreply.github.com>
2024-07-23 19:24:52 +0700

    Fixx -- store.tsx và main.tsx; del store.ts ,

--- Client/src/redux/store.tsx
+++ Client/src/redux/store.tsx
@@ -1,43 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './rootReducer';
-
-const store = configureStore({
-  reducer: rootReducer,
-  // Optionally add middleware if needed
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
-export default store;
-
-
-
-
 // import { configureStore } from '@reduxjs/toolkit';
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage'; 
-// import authReducer from './auth/authSlice';
+// import rootReducer from './rootReducer';
 
-// // Cấu hình Redux Persist
-// const persistConfig = {
-//     key: 'root',
-//     storage, 
-// };
-
-// const persistedReducer = persistReducer(persistConfig, authReducer);
-
-// export const store = configureStore({
-//     reducer: {
-//         auth: persistedReducer,
-//     },
-//     middleware: (getDefaultMiddleware) =>
-//         getDefaultMiddleware({
-//             serializableCheck: false,
-//         }),
+// const store = configureStore({
+//   reducer: rootReducer,
+//   // Optionally add middleware if needed
 // });
-
-// export const persistor = persistStore(store); 
 
 // export type RootState = ReturnType<typeof store.getState>;
 // export type AppDispatch = typeof store.dispatch;
+
+// export default store;
+
+
+
+
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
+import authReducer from './auth/authSlice';
+import categoriesSlice from './categories/categoriesSlice';
+// Cấu hình Redux Persist
+const persistConfig = {
+    key: 'root',
+    storage, 
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+export const store = configureStore({
+    reducer: {
+        auth: persistedReducer,
+        categories: categoriesSlice,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+export const persistor = persistStore(store); 
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
