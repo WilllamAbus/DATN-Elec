11856986cd70b52696f8a1f10add6ac23479d668
huytq4624 <tranquochuy4624@gmail.com>
2024-08-25 13:53:23 +0700

    fix filter-search
    fix filter-search

--- Client/src/components/User/feature/details/comment/comment.tsx
+++ Client/src/components/User/feature/details/comment/comment.tsx
@@ -165,6 +165,7 @@ const Comment = () => {
             <button className="bg-primary border border-primary text-white px-8 py-2 font-medium rounded uppercase flex items-center gap-2 hover:bg-transparent hover:text-primary transition">
               Đăng nhập để bình luận
           </button>
+          
             </Link>
     
           </div>
