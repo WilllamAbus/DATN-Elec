530935d71be1c74e3679352ef06334f3389f941f
huytq4624 <tranquochuy4624@gmail.com>
2024-09-30 21:19:53 +0700

    softdelete cmt - interaction
    softdelete cmt - interaction

--- Client/src/components/User/feature/details/detalsListting/detailListing.tsx
+++ Client/src/components/User/feature/details/detalsListting/detailListing.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 //css carosel sản phẩm liên quan
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination ,} from 'swiper/modules';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
