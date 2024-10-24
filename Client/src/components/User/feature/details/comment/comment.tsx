3499e507a9759154127a058bbb5d2a5a039aacdf
huytq4624 <tranquochuy4624@gmail.com>
2024-10-24 20:18:56 +0700

    thống kê 1
    thống kê 1

--- Client/src/components/User/feature/details/comment/comment.tsx
+++ Client/src/components/User/feature/details/comment/comment.tsx
@@ -238,6 +238,7 @@ const Comment = () => {
           </div>
         ) : (
           <div>
+            
             <Link to="/login">
               <button className="bg-blue-600 border border-blue-600 text-white px-8 py-2 font-medium rounded uppercase flex items-center gap-2 hover:bg-transparent hover:text-blue-600 transition">
                 Đăng nhập để bình luận
