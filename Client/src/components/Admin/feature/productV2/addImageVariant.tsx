6b881a88e8ecb92cd926a32676493d8206fc8988
dophuchau <haudppc07506@fpt.edu.vn>
2024-12-15 08:46:44 +0700

    fix form image

--- Client/src/components/Admin/feature/productV2/addImageVariant.tsx
+++ Client/src/components/Admin/feature/productV2/addImageVariant.tsx
@@ -10,7 +10,7 @@ import { addImageVariantThunk } from "../../../../redux/product/admin/Thunk";
 import { breadcrumbItems, ReusableBreadcrumb } from "../../../../ultils/breadcrumb/admin";
 import { ImageVariant } from "../../../../services/product_v2/admin/types/imageVariant";
 import { Color } from "../../../../services/product_v2/types/attributes/getAllColor";
-import { useImageUpload } from "../../../../hooks/useImageUpload";
+import { useImageUpload } from "./hook/useImageUpload";
 import ColorImageVariantSingle from "./selectVariant/colorImageVariantSingle";
 
 import {
@@ -18,6 +18,7 @@ import {
 } from "./handlersVariant/colorImageVariantSingle";
 import { SingleValue } from "react-select";
 import { Card, CardBody } from "@nextui-org/react";
+import ImageUpload from "./Form/formImage";
 const AddImageVariant: React.FC = () => {
   const {
     register,
@@ -31,7 +32,7 @@ const AddImageVariant: React.FC = () => {
   const productVariantIdString = product_variant_id ?? "";
   const dispatch: AppDispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
-  const { imgPreview, handleImageChange } = useImageUpload();
+  const { imgPreview, handleImageChange, error } = useImageUpload();
   const [selectedColor, setSelectedColor] = useState<SingleValue<Color> | null>(null);
   const onColorChange = (selectedOption: SingleValue<Color> | null) => {
     handleColorImageVariantChange(selectedOption, setSelectedColor, setValue);
@@ -61,41 +62,23 @@ const AddImageVariant: React.FC = () => {
           Thêm hình ảnh cho biến thể
         </h1>
       </div>
-      <Card className='m-4' >
+      <Card className='m-4 shadow-none bg-white border border-gray-100 rounded-lg'>
         <CardBody>
-          <div className="grid grid-cols-[1fr_1fr] px-4 pt-4 xl:grid-cols-[1fr_1fr] xl:gap-4 dark:bg-gray-900">
+          <div className="grid grid-cols-1 px-4 pt-4 gap-4 dark:bg-gray-900">
             <div className="col-span-full xl:col-auto">
-              <div className="p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
-                <div className="items-center sm:flex xl:block 2xl:flex sm:space-x-4 xl:space-x-0 2xl:space-x-4">
-                  {imgPreview && (
-                    <div className="mb-4 rounded-lg w-24 h-24 sm:mb-0 xl:mb-4 2xl:mb-0">
-                      <img src={imgPreview} alt="Image Preview" />
-                    </div>
-                  )}
-                  <div>
-                    <h3 className="mb-1 text-xl font-bold text-gray-900 dark:text-white">Hình ảnh</h3>
-                    <div className="mb-4 text-sm text-gray-500 dark:text-gray-400">
-                      JPG, GIF or PNG. Max size of 800KB
-                    </div>
-
-                    <div className="flex items-center space-x-4">
-                      <input
-                        type="file"
-                        multiple
-                        id="image"
-                        {...register("image")}
-                        onChange={handleImageChange}
-                        className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                      />
-                      {errors.image && <span className="text-red-600">{errors.image.message}</span>}
-                    </div>
-                  </div>
-                </div>
+            <div className="col-span-6 sm:col-span-6">
+              <ImageUpload
+                imgPreview={imgPreview}
+                register={register}
+                handleImageChange={handleImageChange}
+                error={error || errors.image?.message}
+                validation={{ required: "vui lòng chọn ảnh" }}
+              />
               </div>
+
             </div>
             <div className="col-span-full xl:col-auto">
               <div className="p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
-                <h3 className="mb-4 text-xl font-semibold dark:text-white">Tổng quan sản phẩm</h3>
 
                 <div className="grid grid-cols-1 gap-6">
                   <div className="col-span-3 sm:col-span-3">
