10415573c6f995d25a70eda5585fd869f06ba6e3
HIND3110 <daodinhhay@gmail.com>
2024-12-19 21:46:19 +0700

    fix lỗi

--- Client/src/page/User/contact/contact.tsx
+++ Client/src/page/User/contact/contact.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import UserFetAddontacts from "../../../components/User/feature/contact/addContact";
+import UserFetAddcontacts from "../../../components/User/feature/contact/addContact";
 
 const addContacts: React.FC = () => {
   return (
-          <UserFetAddontacts />
+          <UserFetAddcontacts />
   );
 };
 
