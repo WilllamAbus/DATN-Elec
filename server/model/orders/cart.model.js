f3bb5c9c88b886c5e72420ff81c9b696cb2de6be
nmphongk2 <132972138+nmphongk2@users.noreply.github.com>
2024-12-25 17:58:46 +0700

    fix_model cart

--- server/model/orders/cart.model.js
+++ server/model/orders/cart.model.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 const cartItemSchema = new Schema({
-  auctions: { type: Schema.Types.ObjectId, ref: "auctions", required: true },
+  // auctions: { type: Schema.Types.ObjectId, ref: "auctions" },
   product: { type: Schema.Types.ObjectId, ref: "product_v2", required: true },
   productVariant: { type: Schema.Types.ObjectId, ref: "productVariant" },
   quantity: { type: Number, default: 1 },
@@ -11,16 +11,25 @@ const cartItemSchema = new Schema({
   inventory: { type: Schema.Types.ObjectId, ref: "Inventory" },
 });
 const cartItemAuctionSchema = new Schema({
-  auctionWiner: { type: Schema.Types.ObjectId, ref: "AuctionWinner", required: true },
+  auctionWiner: {
+    type: Schema.Types.ObjectId,
+    ref: "AuctionWinner",
+    required: true,
+  },
   auctionStartTime: { type: Date, default: null },
   auctionEndTime: { type: Date, default: null },
+  quantity: { type: Number, default: 1 },
+  price: { type: Number, required: true },
+  totalItemPrice: { type: Number, required: true },
+  isSelected: { type: Boolean, default: false },
+  inventory: { type: Schema.Types.ObjectId, ref: "Inventory" },
 });
 
 const cartSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: "users", required: true },
     items: [cartItemSchema, cartItemAuctionSchema],
-
+    itemAuction: [cartItemAuctionSchema],
     totalPrice: { type: Number, required: true },
     stateNotifi: { type: String, default: "has" },
     isActive: { type: Boolean, default: true },
